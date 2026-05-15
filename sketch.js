5d90f75aa468de5ffa21bd6f1481baaa8a22a454
sean.schallberger@gmail.com <sean.schallberger@gmail.com>
2026-05-15 17:38:11 -0400

    Mountain Backdrop v1 Done - reconsidering colors right now

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ apricotLight = (250, 225, 184)
 apricotMain  = (247, 204, 142)
 apricotDark  = (232, 171, 102)
 
-// Lavender
+// Lavender    // thinking this may be too similar to the deep plum...
 lavenderLight = (217, 196, 240)
 lavenderMain  = (190, 160, 228)
 lavenderDark  = (151, 117, 206)
@@ -45,8 +45,9 @@ inkDark     = (52, 37, 79)
 
 */
 
-// Global Variables
-let bridgeLayer;     // define the new drawable layer
+// Global Variables   // define the new drawable layers
+let backdrop;
+let bridgeLayer;     
 
 
 function setup()
@@ -77,18 +78,22 @@ function draw()
 }
 
 function styled_background(){
+	//setup
+	noStroke();
+
 	//Sun/ Moon
 	fill(247, 204, 142);
 	ellipse(1000, 350, 500, 500);
 
+	// Sun/ Moon Rays
 	noFill();
 	stroke(247, 204, 142);
 	strokeWeight(20);
-	arc(1000, 350, 550, 550, radians(150), radians(200));
-	arc(1000, 350, 550, 550, radians(210), radians(270));
-	arc(1000, 350, 550, 550, radians(280), radians(310));
-	arc(1000, 350, 550, 550, radians(320), radians(30));
-	arc(1000, 350, 550, 550, radians(40), radians(140));
+	arc(1000, 350, 544, 544, radians(150), radians(200));
+	arc(1000, 350, 544, 544, radians(210), radians(270));
+	arc(1000, 350, 544, 544, radians(280), radians(310));
+	arc(1000, 350, 544, 544, radians(320), radians(30));
+	arc(1000, 350, 544, 544, radians(40), radians(140));
 
 	strokeWeight(10);
 	arc(1000, 350, 595, 595, radians(40), radians(140));
@@ -105,12 +110,109 @@ function styled_background(){
 	arc(1000, 350, 650, 650, radians(260), radians(310));
 	arc(1000, 350, 650, 650, radians(320), radians(370));
 
+	arc(1000, 350, 705, 705, radians(0), radians(50));
+	arc(1000, 350, 705, 705, radians(60), radians(120));
+	arc(1000, 350, 705, 705, radians(130), radians(190));
+	arc(1000, 350, 705, 705, radians(200), radians(230));
+	arc(1000, 350, 705, 705, radians(240), radians(300));
+	arc(1000, 350, 705, 705, radians(310), radians(360));
+
+	arc(1000, 350, 830, 830, radians(20), radians(80));
+	arc(1000, 350, 830, 830, radians(90), radians(145));
+	arc(1000, 350, 830, 830, radians(155), radians(215));
+	arc(1000, 350, 830, 830, radians(225), radians(245));
+	arc(1000, 350, 830, 830, radians(255), radians(315));
+	arc(1000, 350, 830, 830, radians(325), radians(380));
+
+	arc(1000, 350, 950, 950, radians(5), radians(55));
+	arc(1000, 350, 950, 950, radians(65), radians(125));
+	arc(1000, 350, 950, 950, radians(135), radians(185));
+	arc(1000, 350, 950, 950, radians(195), radians(235));
+	arc(1000, 350, 950, 950, radians(245), radians(295));
+	arc(1000, 350, 950, 950, radians(305), radians(365));
+		
+	arc(1000, 350, 1050, 1050, radians(25), radians(75));
+	arc(1000, 350, 1050, 1050, radians(85), radians(135));
+	arc(1000, 350, 1050, 1050, radians(145), radians(205));
+	arc(1000, 350, 1050, 1050, radians(215), radians(275));
+	arc(1000, 350, 1050, 1050, radians(285), radians(325));
+	arc(1000, 350, 1050, 1050, radians(335), radians(385));
+
+	arc(1000, 350, 1200, 1200, radians(10), radians(70));
+	arc(1000, 350, 1200, 1200, radians(80), radians(130));
+	arc(1000, 350, 1200, 1200, radians(140), radians(200));
+	arc(1000, 350, 1200, 1200, radians(210), radians(250));
+	arc(1000, 350, 1200, 1200, radians(260), radians(310));
+	arc(1000, 350, 1200, 1200, radians(320), radians(370));
+
+	arc(1000, 350, 1400, 1400, radians(40), radians(140));
+	arc(1000, 350, 1400, 1400, radians(150), radians(210));
+	arc(1000, 350, 1400, 1400, radians(220), radians(240));
+	arc(1000, 350, 1400, 1400, radians(250), radians(260));
+	arc(1000, 350, 1400, 1400, radians(270), radians(320));
+	arc(1000, 350, 1400, 1400, radians(330), radians(30));
+
+	arc(1000, 350, 1650, 1650, radians(20), radians(80));
+	arc(1000, 350, 1650, 1650, radians(90), radians(145));
+	arc(1000, 350, 1650, 1650, radians(155), radians(215));
+	arc(1000, 350, 1650, 1650, radians(225), radians(245));
+	arc(1000, 350, 1650, 1650, radians(255), radians(315));
+	arc(1000, 350, 1650, 1650, radians(325), radians(380));
+
+	// moutains
+	noStroke();
+	fill(190, 160, 228);
+	beginShape();
+	vertex(0, 470);
+	vertex(570, 200);
+	vertex(790, 270);
+	vertex(1250, 550);
+	vertex(1850, 200);
+	vertex(2000, 350);
+	vertex(2000, 1000);
+	vertex(0, 1000);
+	endShape();
+
+	fill(151, 117, 206);
+	beginShape();
+	vertex(0, 470);
+	vertex(275, 340);
+	vertex(650, 520);
+	vertex(310, 324);
+	vertex(387, 286);
+	vertex(650, 420);
+	vertex(418, 272);
+	vertex(487, 240);
+	vertex(1121, 580);
+	vertex(675, 300);
+	vertex(1180, 550);
+	vertex(1250, 555);
+	vertex(1370, 550);
+	vertex(1480, 480);
+	vertex(1570, 450);
+	vertex(1710, 530);
+	vertex(1595, 433);
+	vertex(1735, 312);
+	vertex(1806, 350);
+	vertex(1759, 299);
+	vertex(1863, 225);
+	vertex(2000, 350);
+	vertex(2000, 1000);
+	vertex(0, 1000);
+	endShape();
+
+	fill(217, 196, 240);
+	quad(570, 200, 790, 270, 1185, 512, 767, 280);
+	quad(1250, 550, 1350, 530, 1755, 260, 1825, 213);
+
 }
 
 
 function bridge(){
-	// Background bridge scape
+	// Setup
 	bridgeLayer.noStroke();
+
+	// Background bridge scape
 	bridgeLayer.fill(152, 119, 194);
 	bridgeLayer.rect(0, 395, 2000, 20);
 	bridgeLayer.rect(795, 365, 110, 110, 55);
@@ -200,9 +302,11 @@ function bridge(){
 }
 
 function waterfall(){
+	// Setup
+	noStroke();
+
 	// Waterfall
 	fill(222, 108, 144);
-	noStroke();
 	rect(880, 390, 240, 510, 20);
 
 	fill(241, 145, 170);
